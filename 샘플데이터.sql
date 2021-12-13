3a04c84e8aca81cb8f26e1d8da5834d4822b2729
NGS0916 <xxss05@naver.com>
2021-12-13 14:30:21 +0900

    Update 샘플데이터.sql

--- 샘플데이터.sql
+++ 샘플데이터.sql
@@ -22,4 +22,21 @@ BEGIN
 
 END;
 
+
+DECLARE
+NUM1 NUMBER :=1;
+
+BEGIN
+    WHILE(NUM1<10) --NUM이 10보다 작을때까지 LOOP실행
+    LOOP
+   insert into board_table(writedate,bbno,title,writer,content,membern,name,location,type,replyer) values('19990505',boardseq.nextval,'제목'||boardseq.nextval,'작성자'||boardseq.nextval,'내용',112,'축제이름','강원도','리뷰',3);
+    insert into board_table(writedate,bbno,title,writer,content,membern,name,location,type,replyer) values('19990505',boardseq.nextval,'제목'||boardseq.nextval,'작성자'||boardseq.nextval,'내용',112,'축제이름','경상북도','리뷰',3);
+         insert into board_table(writedate,bbno,title,writer,content,membern,name,location,type,replyer) values('19990505',boardseq.nextval,'제목'||boardseq.nextval,'작성자'||boardseq.nextval,'내용',112,'축제이름','경상남도','리뷰',3);
+            insert into board_table(writedate,bbno,title,writer,content,membern,name,location,type,replyer) values('19990505',boardseq.nextval,'제목'||boardseq.nextval,'작성자'||boardseq.nextval,'내용',112,'축제이름','전라남도','리뷰',3);
+   
+    NUM1 := NUM1+1; --NUM = NUM +1
+    END LOOP;
+
+END;
+
 commit;
